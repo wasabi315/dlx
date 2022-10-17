101ab382705727ce5e7e45c62c9a92b8328f7087
wasabi315 <wasabi.nvim.st@gmail.com>
2022-10-17 21:52:21 +0200

    Use id_arena instead of typed_arena

--- src/lib.rs
+++ src/lib.rs
@@ -1,140 +1,224 @@
 use bit_set::BitSet;
+use id_arena::{Arena, Id};
 use paste::paste;
 use rustc_hash::FxHashMap;
 use std::cell::RefCell;
 use std::collections::HashSet;
 use std::hash::Hash;
-use std::ops::ControlFlow;
-use typed_arena::Arena;
+use std::ops::{ControlFlow, Index, IndexMut};
 
-pub fn solve<L, T, S>(subsets: impl IntoIterator<Item = (L, HashSet<T, S>)>) -> Option<Vec<L>>
-where
-    T: Hash + Eq,
-{
-    let arena = Arena::new();
-    let mut builder = EcpBuilder::new(&arena);
+struct NodeArena(Arena<NodeData>);
 
-    for (label, subset) in subsets {
-        builder.add_subset(label, subset);
-    }
-
-    builder.build().solve()
-}
-
-struct EcpBuilder<'a, L, T> {
-    headers: FxHashMap<T, Node<'a>>,
-    arena: &'a NodeArena<'a>,
-    root: Node<'a>,
-    labels: Vec<L>,
-}
-
-impl<'a, L, T> EcpBuilder<'a, L, T> {
-    fn new(arena: &'a NodeArena<'a>) -> Self {
-        EcpBuilder {
-            headers: FxHashMap::default(),
-            arena,
-            root: Node::new_header(arena),
-            labels: Vec::new(),
-        }
+impl NodeArena {
+    #[inline]
+    fn new() -> Self {
+        NodeArena(Arena::new())
     }
 
     #[inline]
-    fn build(self) -> Ecp<'a, L> {
-        Ecp {
-            dlx: Dlx::new(self.root),
-            labels: self.labels,
-        }
+    fn alloc_header(&mut self) -> Node {
+        Node(self.0.alloc_with_id(|id| {
+            let node = Node(id);
+            NodeData {
+                up: node,
+                down: node,
+                left: node,
+                right: node,
+                header: node,
+                size_or_ix: 0,
+            }
+        }))
+    }
+
+    #[inline]
+    fn alloc(&mut self, ix: usize) -> Node {
+        Node(self.0.alloc_with_id(|id| {
+            let node = Node(id);
+            NodeData {
+                up: node,
+                down: node,
+                left: node,
+                right: node,
+                header: node,
+                size_or_ix: ix,
+            }
+        }))
     }
 }
 
-impl<'a, L, T> EcpBuilder<'a, L, T>
-where
-    T: Hash + Eq,
-{
-    fn add_subset<S>(&mut self, label: L, subset: HashSet<T, S>) {
-        self.labels.push(label);
-        let row_ix = self.labels.len() - 1;
-        let mut row_header: Option<Node> = None;
+impl Index<Node> for NodeArena {
+    type Output = NodeData;
 
-        for elem in subset {
-            let node = Node::new(self.arena, row_ix);
-            if let Some(row_header) = row_header {
-                row_header.insert_left(node);
-            } else {
-                row_header = Some(node);
+    #[inline]
+    fn index(&self, index: Node) -> &Self::Output {
+        self.0.index(index.0)
+    }
+}
+
+impl IndexMut<Node> for NodeArena {
+    #[inline]
+    fn index_mut(&mut self, index: Node) -> &mut Self::Output {
+        self.0.index_mut(index.0)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Node(Id<NodeData>);
+
+#[derive(Clone)]
+struct NodeData {
+    up: Node,
+    down: Node,
+    left: Node,
+    right: Node,
+    header: Node,
+    // size: the number of nodes in a column (when a node is a column header)
+    // ix: the row index (otherwise)
+    size_or_ix: usize,
+}
+
+impl Node {
+    #[inline]
+    fn insert_up(self, arena: &mut NodeArena, node: Node) {
+        let orig_up = std::mem::replace(&mut arena[self].up, node);
+        arena[orig_up].down = node;
+        let mut data = &mut arena[node];
+        data.up = orig_up;
+        data.down = self;
+    }
+
+    #[inline]
+    fn insert_left(self, arena: &mut NodeArena, node: Node) {
+        let orig_left = std::mem::replace(&mut arena[self].left, node);
+        arena[orig_left].right = node;
+        let mut data = &mut arena[node];
+        data.left = orig_left;
+        data.right = self;
+    }
+
+    fn unlink_ud(self, arena: &mut NodeArena) {
+        let NodeData {
+            up, down, header, ..
+        } = arena[self];
+        arena[up].down = down;
+        arena[down].up = up;
+        arena[header].size_or_ix -= 1;
+    }
+
+    fn relink_ud(self, arena: &mut NodeArena) {
+        let NodeData {
+            up, down, header, ..
+        } = arena[self];
+        arena[up].down = self;
+        arena[down].up = self;
+        arena[header].size_or_ix += 1;
+    }
+
+    fn unlink_lr(self, arena: &mut NodeArena) {
+        let NodeData { left, right, .. } = arena[self];
+        arena[left].right = right;
+        arena[right].left = left;
+    }
+
+    fn relink_lr(self, arena: &mut NodeArena) {
+        let NodeData { left, right, .. } = arena[self];
+        arena[left].right = self;
+        arena[right].left = self;
+    }
+}
+
+macro_rules! define_node_iterator {
+    ($dir:ident) => {
+        paste! {
+            struct [<Iter $dir:camel>]<'a> {
+                arena: &'a RefCell<NodeArena>,
+                start: Node,
+                next: Option<Node>,
             }
 
-            let header = *self.headers.entry(elem).or_insert_with(|| {
-                let header = Node::new_header(self.arena);
-                self.root.insert_left(header);
-                header
-            });
-            header.inc_size();
-            node.set_header(header);
-            header.insert_up(node);
+            impl<'a> Iterator for [<Iter $dir:camel>]<'a> {
+                type Item = Node;
+
+                #[inline]
+                fn next(&mut self) -> Option<Self::Item> {
+                    let node = self.next?;
+                    let next = self.arena.borrow()[node].$dir;
+                    self.next = (next != self.start).then(|| next);
+                    Some(node)
+                }
+            }
+
+            impl Node {
+                #[inline]
+                fn [<iter_ $dir>](self, arena: &RefCell<NodeArena>) -> [<Iter $dir:camel>] {
+                    [<Iter $dir:camel>] {
+                        arena,
+                        start: self,
+                        next: Some(self),
+                    }
+                }
+            }
         }
-    }
+    };
 }
 
-struct Ecp<'a, L> {
-    dlx: Dlx<'a>,
-    labels: Vec<L>,
+define_node_iterator! { up }
+define_node_iterator! { down }
+define_node_iterator! { left }
+define_node_iterator! { right }
+
+struct Dlx {
+    arena: RefCell<NodeArena>,
+    root: Node,
 }
 
-impl<'a, L> Ecp<'a, L> {
-    fn solve(mut self) -> Option<Vec<L>> {
-        let label_indices = self.dlx.solve()?;
-        let mut i = 0;
-        self.labels.retain(|_| {
-            i += 1;
-            label_indices.contains(i - 1)
-        });
-        Some(self.labels)
-    }
-}
-
-struct Dlx<'a> {
-    root: Node<'a>,
-}
-
-impl<'a> Dlx<'a> {
-    #[inline]
-    fn new(root: Node<'a>) -> Dlx<'a> {
-        Dlx { root }
-    }
-
+impl Dlx {
     #[inline]
     fn is_solved(&self) -> bool {
-        self.root == self.root.right()
+        self.root == self.arena.borrow()[self.root].right
     }
 
     #[inline]
-    fn min_size_col(&self) -> Option<Node<'a>> {
-        let headers = self.root.iter_right().skip(1);
-        headers.min_by_key(|header| header.size())
+    fn min_size_col(&self) -> Option<Node> {
+        let headers = self.root.iter_right(&self.arena).skip(1);
+        let arena = self.arena.borrow();
+        headers.min_by_key(|header| arena[*header].size_or_ix)
     }
 
-    fn cover(&self, selected_node: Node<'a>) {
-        for node in selected_node.iter_right() {
-            let header = node.header();
-            header.unlink_lr();
+    fn cover(&self, selected_node: Node) {
+        for node in selected_node.iter_right(&self.arena) {
+            let mut arena = self.arena.borrow_mut();
+            let header = arena[node].header;
+            header.unlink_lr(&mut arena);
+            drop(arena);
 
-            for col_node in node.iter_down().skip(1).filter(|node| node != &header) {
-                for row_node in col_node.iter_right().skip(1) {
-                    row_node.unlink_ud();
+            for col_node in node
+                .iter_down(&self.arena)
+                .skip(1)
+                .filter(|node| node != &header)
+            {
+                for row_node in col_node.iter_right(&self.arena).skip(1) {
+                    row_node.unlink_ud(&mut self.arena.borrow_mut());
                 }
             }
         }
     }
 
-    fn uncover(&self, selected_node: Node<'a>) {
-        for node in selected_node.left().iter_left() {
-            let header = node.header();
-            header.relink_lr();
+    fn uncover(&self, selected_node: Node) {
+        let left = self.arena.borrow()[selected_node].left;
+        for node in left.iter_left(&self.arena) {
+            let mut arena = self.arena.borrow_mut();
+            let header = arena[node].header;
+            header.relink_lr(&mut arena);
+            drop(arena);
 
-            for col_node in node.iter_up().skip(1).filter(|node| node != &header) {
-                for row_node in col_node.iter_left().skip(1) {
-                    row_node.relink_ud();
+            for col_node in node
+                .iter_up(&self.arena)
+                .skip(1)
+                .filter(|node| node != &header)
+            {
+                for row_node in col_node.iter_left(&self.arena).skip(1) {
+                    row_node.relink_ud(&mut self.arena.borrow_mut());
                 }
             }
         }
@@ -148,8 +232,8 @@ impl<'a> Dlx<'a> {
 
             let header = dlx.min_size_col().unwrap();
 
-            for node in header.iter_down().skip(1) {
-                let ix = node.ix();
+            for node in header.iter_down(&dlx.arena).skip(1) {
+                let ix = dlx.arena.borrow()[node].size_or_ix;
                 label_indices.insert(ix);
                 dlx.cover(node);
 
@@ -169,177 +253,97 @@ impl<'a> Dlx<'a> {
     }
 }
 
-type NodeArena<'a> = Arena<RefCell<NodeData<'a>>>;
+struct Ecp<L> {
+    dlx: Dlx,
+    labels: Vec<L>,
+}
 
-#[derive(Clone, Copy)]
-struct Node<'a>(&'a RefCell<NodeData<'a>>);
-
-impl<'a> PartialEq for Node<'a> {
-    fn eq(&self, other: &Node<'a>) -> bool {
-        std::ptr::eq(self.0, other.0)
+impl<L> Ecp<L> {
+    fn solve(mut self) -> Option<Vec<L>> {
+        let label_indices = self.dlx.solve()?;
+        let mut i = 0;
+        self.labels.retain(|_| {
+            i += 1;
+            label_indices.contains(i - 1)
+        });
+        Some(self.labels)
     }
 }
 
-#[derive(Clone, Copy)]
-struct NodeData<'a> {
-    up: Option<Node<'a>>,
-    down: Option<Node<'a>>,
-    left: Option<Node<'a>>,
-    right: Option<Node<'a>>,
-    header: Option<Node<'a>>,
-    // size: the number of nodes in a column (when a node is a column header)
-    // ix: the row index (otherwise)
-    size_or_ix: usize,
+struct EcpBuilder<L, T> {
+    arena: NodeArena,
+    headers: FxHashMap<T, Node>,
+    root: Node,
+    labels: Vec<L>,
 }
 
-impl<'a> Node<'a> {
-    #[inline]
-    fn new_header(arena: &'a NodeArena<'a>) -> Self {
-        Node::alloc(arena, 0)
-    }
-
-    #[inline]
-    fn new(arena: &'a NodeArena<'a>, row_ix: usize) -> Self {
-        Node::alloc(arena, row_ix)
-    }
-
-    fn alloc(arena: &'a NodeArena<'a>, size_or_ix: usize) -> Self {
-        let node = Node(arena.alloc(RefCell::new(NodeData {
-            up: None,
-            down: None,
-            left: None,
-            right: None,
-            header: None,
-            size_or_ix,
-        })));
-        let mut node_ref_mut = node.0.borrow_mut();
-        node_ref_mut.up = Some(node);
-        node_ref_mut.down = Some(node);
-        node_ref_mut.left = Some(node);
-        node_ref_mut.right = Some(node);
-        node_ref_mut.header = Some(node);
-        node
-    }
-}
-
-macro_rules! define_node_get_set {
-    ($field:ident) => {
-        paste! {
-            impl<'a> Node<'a> {
-                #[inline]
-                fn $field(&self) -> Node<'a> {
-                    self.0.borrow().$field.unwrap()
-                }
-
-                #[inline]
-                fn [<set_ $field>](&self, node: Node<'a>) {
-                    self.0.borrow_mut().$field = Some(node);
-                }
-            }
+impl<L, T> EcpBuilder<L, T> {
+    fn new() -> Self {
+        let mut arena = NodeArena::new();
+        let root = arena.alloc_header();
+        EcpBuilder {
+            headers: FxHashMap::default(),
+            labels: Vec::new(),
+            arena,
+            root,
         }
-    };
-}
-
-define_node_get_set! { up }
-define_node_get_set! { down }
-define_node_get_set! { left }
-define_node_get_set! { right }
-define_node_get_set! { header }
-
-impl<'a> Node<'a> {
-    #[inline]
-    fn size(&self) -> usize {
-        self.0.borrow().size_or_ix
     }
 
     #[inline]
-    fn inc_size(&self) {
-        self.0.borrow_mut().size_or_ix += 1;
-    }
-
-    #[inline]
-    fn dec_size(&self) {
-        self.0.borrow_mut().size_or_ix -= 1;
-    }
-
-    #[inline]
-    fn ix(&self) -> usize {
-        self.0.borrow().size_or_ix
-    }
-
-    fn insert_up(&self, node: Node<'a>) {
-        node.set_down(*self);
-        node.set_up(self.up());
-        self.up().set_down(node);
-        self.set_up(node);
-    }
-
-    fn insert_left(&self, node: Node<'a>) {
-        node.set_right(*self);
-        node.set_left(self.left());
-        self.left().set_right(node);
-        self.set_left(node);
-    }
-
-    fn unlink_ud(&self) {
-        self.up().set_down(self.down());
-        self.down().set_up(self.up());
-        self.header().dec_size();
-    }
-
-    fn unlink_lr(&self) {
-        self.left().set_right(self.right());
-        self.right().set_left(self.left());
-    }
-
-    fn relink_ud(&self) {
-        self.up().set_down(*self);
-        self.down().set_up(*self);
-        self.header().inc_size();
-    }
-
-    fn relink_lr(&self) {
-        self.left().set_right(*self);
-        self.right().set_left(*self);
-    }
-}
-
-macro_rules! define_node_iterator {
-    ($dir:ident) => {
-        paste! {
-            struct [<Iter $dir:camel>]<'a> {
-                start: Node<'a>,
-                next: Option<Node<'a>>,
-            }
-
-            impl<'a> Iterator for [<Iter $dir:camel>]<'a> {
-                type Item = Node<'a>;
-
-                fn next(&mut self) -> Option<Self::Item> {
-                    let node = self.next?;
-                    let next = node.$dir();
-                    self.next = (next != self.start).then(|| next);
-                    Some(node)
-                }
-            }
-
-            impl<'a> Node<'a> {
-                #[inline]
-                fn [<iter_ $dir>](&self) -> [<Iter $dir:camel>]<'a> {
-                    [<Iter $dir:camel>] {
-                        start: *self,
-                        next: Some(*self),
-                    }
-                }
-            }
+    fn build(self) -> Ecp<L> {
+        Ecp {
+            dlx: Dlx {
+                arena: RefCell::new(self.arena),
+                root: self.root,
+            },
+            labels: self.labels,
         }
-    };
+    }
 }
 
-define_node_iterator! { up }
-define_node_iterator! { down }
-define_node_iterator! { left }
-define_node_iterator! { right }
+impl<L, T> EcpBuilder<L, T>
+where
+    T: Hash + Eq,
+{
+    fn add_subset<S>(&mut self, label: L, subset: HashSet<T, S>) {
+        self.labels.push(label);
+        let row_ix = self.labels.len() - 1;
+        let mut row_header: Option<Node> = None;
+
+        let arena = &mut self.arena;
+        for elem in subset {
+            let node = arena.alloc(row_ix);
+            if let Some(row_header) = row_header {
+                row_header.insert_left(arena, node);
+            } else {
+                row_header = Some(node);
+            }
+
+            let header = *self.headers.entry(elem).or_insert_with(|| {
+                let header = arena.alloc_header();
+                self.root.insert_left(arena, header);
+                header
+            });
+            arena[header].size_or_ix += 1;
+            arena[node].header = header;
+            header.insert_up(arena, node);
+        }
+    }
+}
+
+#[inline]
+pub fn solve<L, T, S>(subsets: impl IntoIterator<Item = (L, HashSet<T, S>)>) -> Option<Vec<L>>
+where
+    T: Hash + Eq,
+{
+    let mut builder = EcpBuilder::new();
+
+    for (label, subset) in subsets {
+        builder.add_subset(label, subset);
+    }
+
+    builder.build().solve()
+}
 
 #[cfg(test)]
 mod test {
