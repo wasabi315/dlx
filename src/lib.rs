01f1bd55de2af7330f9db178cbeb6ab3e7f4eb6a
wasabi315 <wasabi.nvim.st@gmail.com>
2022-11-03 18:45:31 +0100

    Add test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 use std::hash::Hash;
 
+mod dlx;
 mod node;
 mod problem;
-mod dlx;
 mod solver;
 
 use node::NodeArena;
@@ -26,7 +26,7 @@ where
 
 #[cfg(test)]
 mod test {
-    use super::solve;
+    use super::*;
 
     macro_rules! ecp {
         ($($label:expr => {$($elem:expr),*},)*) => {
@@ -71,5 +71,56 @@ mod test {
             () => {4},
         };
         assert_eq!(solve(ecp), None);
+    }
+
+    #[test]
+    fn test4() {
+        let arena = NodeArena::new();
+        let ecp = Problem::<(), ()>::new(&arena);
+        let mut solutions = Solver::new(ecp).solutions();
+        assert_eq!(solutions.next(), Some(vec![]));
+        assert_eq!(solutions.next(), None);
+    }
+
+    #[test]
+    fn test5() {
+        let arena = NodeArena::new();
+        let mut ecp = Problem::<i32, ()>::new(&arena);
+        ecp.add_subset(1, HashSet::from([]));
+        ecp.add_subset(2, HashSet::from([]));
+        ecp.add_subset(3, HashSet::from([]));
+        let mut solutions = Solver::new(ecp).solutions();
+        assert_eq!(solutions.next(), Some(vec![]));
+        assert_eq!(solutions.next(), None);
+    }
+
+    #[test]
+    fn test6() {
+        let arena = NodeArena::new();
+        let mut ecp = Problem::<i32, ()>::new(&arena);
+        ecp.add_subset(1, HashSet::from([()]));
+        ecp.add_subset(2, HashSet::from([()]));
+        ecp.add_subset(3, HashSet::from([()]));
+        let mut solutions = Solver::new(ecp).solutions();
+        assert_eq!(solutions.next(), Some(vec![1]));
+        assert_eq!(solutions.next(), Some(vec![2]));
+        assert_eq!(solutions.next(), Some(vec![3]));
+        assert_eq!(solutions.next(), None);
+    }
+
+    #[test]
+    fn test7() {
+        let arena = NodeArena::new();
+        let mut ecp = Problem::<i32, bool>::new(&arena);
+        ecp.add_subset(1, HashSet::from([true]));
+        ecp.add_subset(2, HashSet::from([true]));
+        ecp.add_subset(3, HashSet::from([false]));
+        ecp.add_subset(4, HashSet::from([false]));
+        let mut solutions = Solver::new(ecp).solutions();
+        assert_eq!(solutions.next(), Some(vec![1, 3]));
+        assert_eq!(solutions.next(), Some(vec![1, 4]));
+        assert_eq!(solutions.next(), Some(vec![2, 3]));
+        assert_eq!(solutions.next(), Some(vec![2, 4]));
+        assert_eq!(solutions.next(), None);
     }
 }
